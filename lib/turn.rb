a6d2038e1f0d41fd62166909408a710b8b1b24fd
Julian Brown <julianxbrown@gmail.com>
2018-11-11 18:25:28 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+end 
+
+def valid_move(board,index)
+  if position_taken?
+        execute something
+    else
+        execute something else
+    end
+end
+end
+
+def move(board)
+  
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+end
+
+   
+  
